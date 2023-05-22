8f6a6f2c8d2379cbb5c25330e58fa6ddbee14b54
SEAF39 <labber@DESKTOP-7J1C764>
2023-05-21 22:10:17 -0400

    Updating the App Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 /* App.js */
-import logo from './logo.svg';
-import './App.css';
+
+
+import "./App.css";
+import PlayerList from "./components/PlayerList";
+import MatchList from "./components/MatchList";
 
 function App() {
   return (
     <div className="App">
-    
+      <h1>
+        Tourney Matches{" "}
+        <span>Where Coding and Tournaments found their Match!</span>
+      </h1>
+      <PlayerList />
+      <MatchList />
     </div>
   );
 }
