3ef991740df7670da7bfd8927a0d833b5bf9cc1a
SEAF39 <labber@DESKTOP-7J1C764>
2023-05-21 22:06:34 -0400

    add player & list & match + list

---
+++ src/components/PlayerList.js
@@ -0,0 +1,17 @@
+/* PlayerList.js */
+
+// PlayerList.js
+
+import React from "react"; //optional
+import Player from "./Player";
+
+function PlayerList() {
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      <Player />
+    </section>
+  );
+}
+
+export default PlayerList;
