a74547e29145445e00adcabce0d33f000e8a2910
SEAF39 <labber@DESKTOP-7J1C764>
2023-05-22 02:35:22 -0400

    Render a single match

---
+++ src/components/MatchList.jsx
@@ -0,0 +1,23 @@
+/* MatchList.jsx */
+import React from "react";
+import Match from "./Match";
+import matchData from "../data/matchData";
+
+function MatchList(props) {
+  return (
+    <section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      {matchData.map((match) => (
+        <Match
+          key={match.matchNumber}
+          players={match.players}
+          winner={match.winner}
+          scoreDifference={match.scoreDifference}
+        />
+      ))}
+    </section>
+  );
+}
+
+export default MatchList;
+
