ff9f905ce4aa1aec685ec082f183291144dcefbc
SEAF39 <labber@DESKTOP-7J1C764>
2023-05-22 02:37:09 -0400

    Receiving The Props For A Player

---
+++ src/components/PlayerList.jsx
@@ -0,0 +1,28 @@
+/* PlayerList.jsx */
+import React from "react";
+import matchData from "../data/matchData";
+import playerData from "../data/playerData";
+import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
+import Player from "./Player";
+
+function PlayerList() {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      {parsedPlayerData.map((player) => (
+        <Player
+          key={player.gamerTag}
+          gamerTag={player.gamerTag}
+          firstName={player.firstName}
+          lastName={player.lastName}
+          wins={player.wins}
+        />
+      ))}
+    </section>
+  );
+}
+
+export default PlayerList;
